de898bd999962855f14f07f3c7fa622b1dca1cdd
lloydliu717 <lloydliu@bu.edu>
2017-03-23 13:18:31 -0400

    creat product placement for year 2015 and period 1 and 2

--- R/Diary.R
+++ R/Diary.R
@@ -24,41 +24,66 @@ tmp[,Period := stri_sub(tmp$NEWID,from=-1,to=-1)]
 tmp[,buy := 1]
 tmp[,NEWID := NULL]
 
+period1 = subset(tmp, subset = Period == 1)
+period2 = subset(tmp, subset = Period == 2)
+
 tmp = tmp[,list(buy = sum(buy)),by=list(CUID,UCC)]
+
+tmp1 = period1[,list(buy = sum(buy)),by=list(CUID,UCC)]
+tmp2 = period2[,list(buy = sum(buy)),by=list(CUID,UCC)]
+
 
 id = unique(tmp$CUID)
 
+id1 = unique(tmp1$CUID)
+id2 = unique(tmp2$CUID)
+
+
 category = as.factor(unique(tmp$UCC))
-table1 = as.data.frame(matrix(nrow = 5785, ncol = 488))
+
+table1 = as.data.frame(matrix(nrow = length(id1), ncol = length(category)))
+table2 = as.data.frame(matrix(nrow = length(id2), ncol = length(category)))
+
+
 names(table1) = category
-rownames(table1) = id
+names(table2) = category
+
+rownames(table1) = id1
+rownames(table2) = id2
 
 
-
-for(cuid in id){
-  index = which(rownames(table1)==cuid)
-  tmprow = subset(tmp, subset = CUID == cuid) %>% spread(UCC,buy)
-  a = match(names(table1), names(tmprow))
+for(cuid in id2){
+  index = which(rownames(table2)==cuid)
+  tmprow = subset(tmp2, subset = CUID == cuid) %>% spread(UCC,buy)
+  a = match(names(table2), names(tmprow))
   index1 = which(!is.na(a))
   tmprow = as.matrix(tmprow)
   for(j in index1){
-    table1[index, j] = tmprow[,a[j]]
-    print(j)
+    table2[index, j] = tmprow[,a[j]]
+#    print(j)
   }
   print(index)
 }
 
-table1[is.na(table1)] = 0
+table2[is.na(table2)] = 0
+table2 = data.matrix(table2)
+table2 = as.data.frame(table2)
 
 
 ## follow part do not work
-ucc = readLines("DATA/csxdstub.txt")
-ucc = as.data.frame(ucc)
-ucc = as.character(ucc)
-for(i in 1:nrow(ucc)){
+ucc = read.csv("DATA/ucc_existed.csv",stringsAsFactors = F)
+
+category = as.numeric(as.character(category))
+category1 = NULL
+for(i in 1:length(category)){
+  if(sum(ucc$UCC==category[i])==0){
+    category1[i] = category[i]
+  }
+  else {
+    category1[i] = ucc$PRODUCT[ucc$UCC==category[i]]
+  }
   print(i)
-  ucc$ucc[i] = as.character(ucc$ucc[i])
 }
 
-t = sub(pattern = "  ",replacement = ",",t)
-t = as.character(ucc$ucc[1])
+names(table2) = category1
+write.csv(table2,file = "product_placement_2015_period2.csv")
